cfc4ddd050d8f342fc34b26e1c410891b2e2e788
Aje Damilola <ajedamilola2005@gmail.com>
2024-09-12 15:50:55 +0100

    More changes

--- src/pages/posts/posts.jsx
+++ src/pages/posts/posts.jsx
@@ -3,9 +3,11 @@ import PostCard from "../components/PostCard"
 function Posts() {
     return (
         <div>
-            {[1, 2, 3].map(p => {
-                return <PostCard key={p} />
-            })}
+            <div className="max-w-2xl mx-auto">
+                {[1, 2, 3].map(p => {
+                    return <PostCard key={p} />
+                })}
+            </div>
         </div>
     )
 }
