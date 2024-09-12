08411290ef4b8fda21756720fcd3296d1c02d9d1
micheal-Oluwafemi <raytech401@gmail.com>
2024-09-12 13:51:24 +0100

    Fixess

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -3,9 +3,13 @@ const Navbar = () => {
   return (
     <div className="p-10 flex items-center justify-between">
         <div>
-            <h1 className="text-3xl font-bold text-neutral-100">Dechat</h1>
+            <h1 className="text-3xl font-bold text-neutral-200">Dechat</h1>
         </div>
-        <div></div>
+
+
+        <div>
+           <button className="px-4 py-2 bg-pink-800 text-white rounded-full">Connect your wallet</button>
+        </div>
     </div>
   )
 }
