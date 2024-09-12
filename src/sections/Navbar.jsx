5af83b770c02420f9f051d3ae36e5ab5232892f0
micheal-Oluwafemi <raytech401@gmail.com>
2024-09-12 13:35:16 +0100

    Updates...

---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,13 @@
+
+const Navbar = () => {
+  return (
+    <div className="p-10 flex items-center justify-between">
+        <div>
+            <h1 className="text-3xl font-bold text-neutral-100">Dechat</h1>
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Navbar
